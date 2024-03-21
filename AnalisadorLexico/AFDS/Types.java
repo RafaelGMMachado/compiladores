3e6eafd30b3fefc31319f37a5b714075daaa3bc0
Rafael <rafael.gmm@outlook.com>
2024-03-21 19:28:43 -0300

    Altera formato de busca de tipos

--- AnalisadorLexico/AFDS/Types.java
+++ AnalisadorLexico/AFDS/Types.java
@@ -4,23 +4,26 @@ import java.util.List;
 import java.util.ArrayList;
 
 import Token.Token;
+import Utility.Dicionario;
 
 public class Types extends AFD{
 
     @Override
     public Token evaluate(CharacterIterator code){
-        List<String> reservadas = new ArrayList<>();
-        reservadas.add("int");
-        reservadas.add("return");
-        reservadas.add("float");
+        List<Dicionario> reservadas = new ArrayList<>();
 
-        for (String res : reservadas){
-            for (char c : res.toCharArray()){
+        reservadas.add(new Dicionario("double", "doseDupla"));
+        reservadas.add(new Dicionario("float", "dose"));
+        reservadas.add(new Dicionario("short", "shot"));
+        reservadas.add(new Dicionario("int", "latinha"));
+
+        for (Dicionario reservada : reservadas){
+            for (char c : reservada.valor.toCharArray()){
                 if (code.current() == c)
                     code.next();   
             }
             if (endPalavra(code)){
-                return new Token("RESERVADA_" + res.toUpperCase(), res);
+                return new Token("RESERVADA_" + reservada.chave.toUpperCase(), reservada.valor);
             }
         }
         return null;
