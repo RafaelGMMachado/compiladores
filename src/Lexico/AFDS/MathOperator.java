5f15d237e4b89a06eeedc76473703d2bb5a2b9ac
Rafael Machado <95395357+RafaelGMMachado@users.noreply.github.com>
2024-05-02 21:48:45 -0300

    Alteração na estrutura das pastas e pacotes

---
+++ src/Lexico/AFDS/MathOperator.java
@@ -0,0 +1,35 @@
+package AFDS;
+import java.text.CharacterIterator;
+
+import Token.Token;
+
+public class MathOperator extends AFD{
+    
+    @Override
+    public Token evaluate(CharacterIterator code){
+
+        switch (code.current()){
+            case '+':
+                code.next();
+                return new Token("OPERADOR_MAIS", "+");
+            case '-':
+                code.next();
+                return new Token("OPERADOR_MENOS", "-");
+            case '*':
+                code.next();
+                    
+                if (code.current() == '*') {
+                    code.next();
+                    return new Token("OPERADOR_VEZES", "**");
+                }
+                
+                else return new Token("OPERADOR_EXPONENCIACAO", "*");
+            case '/':
+                code.next();
+                return new Token("OPERADOR_DIVIDIR", "/");
+
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/Lexico/Token/Token.java
@@ -0,0 +1,24 @@
+package Token;
+
+public class Token {
+    String tipo;
+    String lexema;
+
+    public Token(String tipo, String lexema){
+        this.lexema = lexema;
+        this.tipo = tipo;
+    }
+
+    public String getLexema(){
+        return lexema;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + tipo + ", " + lexema + ">";
+    }
+}
