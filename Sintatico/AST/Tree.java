1e4832fa9f716080656c6bdc65fdc48e29147b1d
Rafael <rafael.gmm@outlook.com>
2024-05-18 19:21:51 -0300

    Adiciona métodos para printar a arvore a partir da raiz

--- Sintatico/AST/Tree.java
+++ Sintatico/AST/Tree.java
@@ -8,12 +8,30 @@ public class Tree {
         this.root = root;
     }
 
+    public void simpleWalk (){
+        System.out.println(" " + root.data + " ");
+
+        for (Node child: root.getChildren()) {
+            simpleWalk(child);
+        }
+    }
+
     public void simpleWalk (Node node){
         System.out.println(" " + node.data + " ");
 
         for (Node child: node.getChildren()) {
             simpleWalk(child);
         }
+    }
+
+    public void walk(){
+        System.out.println(root.enter);
+        if (root.isLeaf())
+            System.out.println(" " + root.data + " ");
+        for (Node child: root.getChildren())
+            walk(child);
+
+        System.out.println(root.exit);
     }
 
     public void walk(Node node){
@@ -25,6 +43,5 @@ public class Tree {
 
         System.out.println(node.exit);
     }
-
 
 }
