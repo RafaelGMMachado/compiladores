bbfb2b0e0c17755e2925646ccace0cc953c2c2f5
Rafael <rafael.gmm@outlook.com>
2024-05-19 17:57:32 -0300

    Teste bem sucedido

--- output.c
+++ output.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(){
- int  i  =  0  ;  while  (  i  <  10  )  {  printf  (  "  FOII  "  )  ;  }    
-return 0;
-} 
+int main() {
+    int a ;
+    int b ;
+    int c ;
+    float d ;
+    printf ( " Programa Teste " ) ;
+    printf ( " Digite A " ) ;
+    scanf ( " %d ", &a ) ;
+    printf ( " Digite B " ) ;
+    scanf ( " %d ", &b ) ;
+    if ( a < b ) {
+        c = a + b ;
+    }
+    else {
+        c = a - b ;
+    }
+    printf ( " C e igual a " ) ;
+    printf ( " %d ", c ) ;
+    d = c / ( a + b ) ;
+    printf ( " D e igual a " ) ;
+    printf ( " %f ", d ) ;
+    return 0;
+}
