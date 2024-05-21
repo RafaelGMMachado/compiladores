27c76fb4e8da57793eb7df9ffccc636de18e8753
Rafael <rafael.gmm@outlook.com>
2024-05-21 01:23:47 -0300

    Adiciona escolha de arquivos

--- compile.sh
+++ compile.sh
@@ -1,24 +1,27 @@
-#---------------------------- MAIN --------------------------------
-Main()
-{	
-	javac *.java
-	java Main
-	astyle --quiet output.c
-	gcc output.c
-	./a.out
-}
-
 #---------------------------- EXECUÇÃO --------------------------------
+if [ -z "$1"];then
+	echo -e "\033[01;31m[!] Nenhum arquivo passado para compilação [!]\033[0m"
+	exit 1
+fi
+
 if [[  ! $(dpkg -l astyle) ]]
 then
 	echo -e "\033[01;31m[!] Instalando dependencias... [!]\033[0m"
 	sudo apt install -y astyle;
 	echo " ********************** "
 	echo -e "\033[01;31m[!] Starting the Compiling process... [!]\033[0m"
-	Main
+	javac *.java
+	java Main.java "$1"
+	astyle --quiet output.c
+	gcc output.c
+	./a.out
 	exit 0
 else
 	echo -e "\033[01;31m[!] Starting the Compiling process... [!]\033[0m"
-	Main
+	javac *.java
+	java Main.java "$1"
+	astyle --quiet output.c
+	gcc output.c
+	./a.out
 	exit 0
 fi
