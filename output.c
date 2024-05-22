d0150d89a81cce1c445f689859d2f9f3249dae8d
Rafael <rafael.gmm@outlook.com>
2024-05-21 21:28:37 -0300

    Altera arquivo de teste e deleta antigos

--- output.c
+++ output.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-int a ; int b ; int c ; float d ; printf ( "Programa Teste\n" ) ; printf ( "Digite A\n" ) ; scanf ( "%d" , &a ) ; printf ( "Digite B\n" ) ; scanf ( "%d" , &b ) ; if ( a < b ) { c = a +b ; } else { c = a -b ; } printf ( "C e igual a\n" ) ; printf ( "%d" , c ) ; d = c /( a +b ) ; printf ( "D e igual a\n" ) ; printf ( "%f" , d ) ; 
+int a ; int b ; int c ; double d ; printf ( "Programa Teste\n" ) ; printf ( "Digite A\n" ) ; scanf ( "%d" , &a ) ; printf ( "Digite B\n" ) ; scanf ( "%d" , &b ) ; if ( a < b ) { c = a +b ; } else { c = a -b ; } printf ( "C e igual a\n" ) ; printf ( "%d" , c ) ; d = c /( a +b ) ; printf ( "\n" ) ; printf ( "D e igual a\n" ) ; printf ( "%f" , d ) ; 
 return 0;
 } 
